bf2cd372a68a90b3fbeb883d6f631ac8dd5c1223
TanmayKhurana <74850207+TanmayKhurana@users.noreply.github.com>
2021-02-02 00:17:30 +0530

    Add files via upload

---
+++ myLibrary.js
@@ -0,0 +1,100 @@
+function touching(car1, object1){
+
+  if (object1.x-car1.x<=(car1.width+object1.width)/2) {
+    car1.velocityX = 0;
+    var damage = 0.5*weight1*speed1*speed1/(thickness*thickness*thickness);
+  if (damage > 25){
+    car1.shapeColor = "red"
+   }
+   else{ if(damage < 5){
+     car1.shapeColor = "yellow"
+     }
+   }
+
+  if (damage < 5 && damage> 2){
+    car1.shapeColor = "yellow"
+   }
+   else{ if(damage > 25){
+    car1.shapeColor = "red"
+   }
+  }
+  if (damage < 2){
+    car1.shapeColor = "blue"
+   }
+  }
+  }
+
+  function touching(car2, object2){
+    if (object2.x-car2.x<=(car2.width+object2.width)/2) {
+      car2.velocityX = 0;
+      var damage = 0.5*weight2*speed2*speed2/(thickness*thickness*thickness);
+    if (damage > 25){
+      car2.shapeColor = "red"
+     }
+     else{ if(damage < 5){
+       car2.shapeColor = "yellow"
+       }
+     }
+  
+    if (damage < 5 && damage> 2){
+      car2.shapeColor = "yellow"
+     }
+     else{ if(damage > 25){
+      car2.shapeColor = "red"
+     }
+    }
+    if (damage < 2){
+      car2.shapeColor = "blue"
+     }
+    }
+  }  
+  
+    function touching(car3, object3){
+      if (object3.x-car3.x<=(car3.width+object3.width)/2) {
+        car3.velocityX = 0;
+        var damage = 0.5*weight3*speed3*speed3/(thickness*thickness*thickness);
+      if (damage > 25){
+        car3.shapeColor = "red"
+       }
+       else{ if(damage < 5){
+         car3.shapeColor = "yellow"
+         }
+       }
+    
+      if (damage < 5 && damage> 2){
+        car3.shapeColor = "yellow"
+       }
+       else{ if(damage > 25){
+        car3.shapeColor = "red"
+       }
+      }
+      if (damage < 2){
+        car3.shapeColor = "blue"
+       }
+      }
+        }
+
+        function touching(car4, object4){
+          if (object4.x-car4.x<=(car4.width+object4.width)/2) {
+            car4.velocityX = 0;
+            var damage = 0.5*weight4*speed4*speed4/(thickness*thickness*thickness);
+          if (damage > 25){
+            car4.shapeColor = "red"
+           }
+           else{ if(damage < 5){
+             car4.shapeColor = "yellow"
+             }
+           }
+        
+          if (damage < 5 && damage> 2){
+            car4.shapeColor = "yellow"
+           }
+           else{ if(damage > 25){
+            car4.shapeColor = "red"
+           }
+          }
+          if (damage < 2){
+            car4.shapeColor = "blue"
+           }
+          }
+            }       
---
+++ sketch.js
@@ -0,0 +1,83 @@
+var wall1, wall2, wall3, wall4
+var bullet1, bullet2, bullet3, bullet4, thickness
+var speed1, speed2, speed3, speed4, weight1, weight2, weight3, weight4 
+
+var sidewall1, sidewall2, sidewall3, sidewall4
+
+function setup() {
+  createCanvas(1600,500);
+  
+  speed1 = random(50, 250)
+  weight1 = random(50, 100)
+
+  speed2 = random(50, 250)
+  weight2 = random(50, 100)
+
+  speed3 = random(50, 250)
+  weight3 = random(50, 100)
+
+  speed4 = random(50, 250)
+  weight4 = random(50, 100)
+
+  bullet1 = createSprite(-10, 50, 20, 10)
+  bullet1.shapeColor = "white"
+  bullet1.velocityX = speed1
+
+  bullet2 = createSprite(-20, 160, 20, 10)
+  bullet2.shapeColor = "white"
+  bullet2.velocityX = speed2
+
+  bullet3 = createSprite(-10, 270, 20, 10)
+  bullet3.shapeColor = "white"
+  bullet3.velocityX = speed3
+
+  bullet4 = createSprite(-20, 380, 20, 10)
+  bullet4.shapeColor = "white"
+  bullet4.velocityX = speed4
+
+  thickness = random(20, 100)
+  
+  sidewall1 = createSprite(1000, 105, 10000, 10)
+  sidewall1.shapeColor = "white"
+  wall1 = createSprite(1450, 50, thickness, height/5)
+
+  sidewall2 = createSprite(1000, 215, 10000, 10)
+  sidewall2.shapeColor = "white"
+  wall2 = createSprite(1450, 160, thickness, height/5)
+
+  sidewall3 = createSprite(1000, 325, 10000, 10)
+  sidewall3.shapeColor = "white"
+  wall3 = createSprite(1450, 270, thickness, height/5)
+
+  sidewall4 = createSprite(1000, 435, 10000, 10)
+  sidewall4.shapeColor = "white"
+  wall4 = createSprite(1450, 380, thickness, height/5)
+}
+
+function draw() {
+  background("black");
+  
+  wall1.depth = bullet1.depth - 1
+  wall2.depth = bullet2.depth - 1
+  wall3.depth = bullet3.depth - 1
+  wall4.depth = bullet4.depth - 1
+
+  textSize(30);
+  stroke("yellow");
+  text("Moderate", 700,480);
+  
+  textSize(30);
+  stroke("blue");
+  text("Strong", 200,480);
+
+  textSize(30);
+  stroke("red");
+  text("Weak", 1200,480);
+
+  touching(bullet1, wall1);
+  touching(bullet2, wall2);
+  touching(bullet3, wall3);
+  touching(bullet4, wall4);
+
+  drawSprites();
+}
